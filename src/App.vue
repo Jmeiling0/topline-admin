00aa92d1d879af313d517dde29cbe8b937b4840c
Jmeiling0 <1747673052@qq.com>
2019-06-20 21:49:40 +0800

    初始化项目结构

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <p>App component</p>
+  </div>
+</template>
+<script>
+export default {
+  name: 'App',
+  data () {
+    return {}
+  }
+}
+</script>
+<style lang="less" scope>
+</style>
