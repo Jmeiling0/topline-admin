f12ce4b79924d2a30613c17777bb70c5da69c5c3
Jmeiling0 <1747673052@qq.com>
2019-06-27 11:39:13 +0800

    使用路由拦截器处理页面访问权限

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,7 @@ import Router from 'vue-router'
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
   routes: [
   //   {
   //   name: 'home',
@@ -34,3 +34,35 @@ export default new Router({
     }
   ]
 })
+
+/****
+ * 所有的路由都要经过这里
+ * to 去哪儿
+ * from 来自哪里
+ * next 允许通过的方法
+ *
+ */
+
+router.beforeEach((to, from, next) => {
+  const userInfo = window.localStorage.getItem('user_info')
+  // 如果是非 /login页面,判断其登陆的状态
+  if (to.path !== '/login') {
+    // 如果没有登陆,让其跳转到登陆页
+    if (!userInfo) {
+      return next({ name: 'login' })
+    } else {
+      // 如果登陆了,则允许通过
+      next()
+    }
+  } else {
+    // 如果的登陆了,就不允许访问登陆页面了
+    if (userInfo) {
+      next(false)
+    } else {
+    // 没有登录,才允许访问登陆页面
+      next()
+    }
+  }
+})
+// 导出
+export default router
